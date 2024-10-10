a2885a86b879db6937fbd20707c3ee4311e61746
Ana4267 <ana.oliveira@be-enlighten.com>
2024-10-10 08:43:52 -0300

    Cadastro de Usuários e Autenticação com JWT e Bcrypt

--- server.js
+++ server.js
@@ -1,23 +1,8 @@
-const express = require('express');
-const sqlite3 = require('sqlite3').verbose();
-const app = express();
-const db = new sqlite3.Database('./lista-tarefas.db');
+const app = require('./app');
+const PORT = 3000;
 
-// Middleware para ler JSON
-app.use(express.json());
-
-// Rota POST para adicionar tarefa
-app.post('/tarefas', (req, res) => {
-    const { tarefa } = req.body;
-    db.run("INSERT INTO tarefas (tarefa) VALUES (?)", [tarefa], function(err) {
-        if (err) {
-            return res.status(500).json({ error: err.message });
-        }
-        res.status(201).json({ id: this.lastID, tarefa });
-    });
+app.listen(PORT, () => {
+   console.log(`Server is running on port ${PORT}`);
 });
 
-// Iniciar o servidor
-app.listen(3000, () => {
-    console.log('Servidor rodando na porta 3000');
-});
+module.exports = app;
